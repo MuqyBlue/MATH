d272dd92bcde10a671302ab24df31f9218cef2dd
MuqyBlue <muqyaswork@gmail.com>
2026-04-09 17:46:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ document.addEventListener("DOMContentLoaded", () => {
     html2canvas(document.querySelector(".container")).then(canvas => {
 
       const link = document.createElement("a");
-      link.download = "worksheet.png";
+      link.download = `worksheet-${Date.now()}.png`
       link.href = canvas.toDataURL("image/png");
       link.click();
 
